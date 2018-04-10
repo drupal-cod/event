f660d14ff4b5ea9e68913cd813e67c27d548e42e
Jakob Perry <japerry@45640.no-reply.drupal.org>
2018-04-10 12:17:50 -0500

    Remove the commented out options here.

--- src/Entity/EventInterface.php
+++ src/Entity/EventInterface.php
@@ -14,7 +14,6 @@ use Drupal\user\EntityOwnerInterface;
  */
 interface EventInterface extends ContentEntityInterface, RevisionLogInterface, EntityChangedInterface, EntityOwnerInterface {
 
-  // Add get/set methods for your configuration properties here.
   /**
    * Gets the Event name.
    *
